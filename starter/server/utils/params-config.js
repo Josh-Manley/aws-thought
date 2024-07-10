584f36d79d9ebad69777b60d9a77926a06d8d0c4
Josh-Manley <joshmanley45@gmail.com>
2024-07-10 15:46:28 -0600

    finished rendering images to the browser

--- starter/server/utils/params-config.js
+++ starter/server/utils/params-config.js
@@ -1,6 +1,6 @@
 const { v4: uuidv4 } = require('uuid');
 
-const params = (fileName) => {
+const params = fileName => {
   const myFile = fileName.originalname.split('.');
   const fileType = myFile[myFile.length - 1];
 
@@ -8,6 +8,7 @@ const params = (fileName) => {
     Bucket: 'user-images-0eee11f4-0476-48fd-afd7-3617b19f9b94',
     Key: `${uuidv4()}.${fileType}`,
     Body: fileName.buffer,
+    ACL: 'public-read',
   };
 
   return imageParams;
